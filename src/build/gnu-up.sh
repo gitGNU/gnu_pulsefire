5778743c96c17060be724c46e3a95df9367b46b8
Willem Cazander <w.cazander@gmail.com>
2012-02-22 12:33:15 +0100

    Made gnu-up.sh work.

--- src/build/gnu-up.sh
+++ src/build/gnu-up.sh
@@ -3,11 +3,11 @@
 # Small script to sign and upload files to savannah
 #
 
-if ["" == "$1"]; then
+if [ "" == "$1" ]; then
 	echo "No username given to upload.";
 	exit 1;
 fi;
-if ["" == "$2"];then
+if [ "" == "$2" ];then
 	echo "No upload dir given.";
 	exit 1;
 fi;
@@ -15,21 +15,23 @@ fi;
 # Goto project root;
 cd `dirname $0`/../..;
 
-# Sign files we want to upload.
-#gpg -b --use-agent pulsefire-build/pulsefire-build-ui-launch4j/*.zip;
-#gpg -b --use-agent pulsefire-build/pulsefire-build-ui-dist/target/*-bin.*;
-#gpg -b --use-agent pulsefire-build/pulsefire-build-project-src/target/*-dist.*;
-
-# Make sure readable
-chmod 644 pulsefire-build/pulsefire-build-ui-launch4j/*;
-chmod 644 pulsefire-build/pulsefire-build-ui-dist/target/*;
-chmod 644 pulsefire-build/pulsefire-build-project-src/target/*;
-
+# Copy to one new dir.
 mkdir -p target/gnu-up/$2;
 mv pulsefire-build/pulsefire-build-ui-launch4j/target/*.zip target/gnu-up/$2;
-mv pulsefire-build/pulsefire-build-ui-launch4j/target/*.zip.sig target/gnu-up/$2;
 mv pulsefire-build/pulsefire-build-ui-dist/target/*-bin.* target/gnu-up/$2;
 mv pulsefire-build/pulsefire-build-project-src/target/*-dist.* target/gnu-up/$2;
 
+# Sign per file we want to upload.
+for FILE in `ls target/gnu-up/$2/*`; do
+	gpg -b --use-agent $FILE;
+done;
+
+# Make sure readable
+chmod 644 target/gnu-up/$2/*;
+
+# And copy with new dir to gnu
 scp -r target/gnu-up/$2 $1@dl.sv.nongnu.org:/releases/pulsefire/;
+
+echo "Done";
+exit 0;
 
